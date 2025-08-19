11bb48bc93402629bb60363565f84a6b1c4b9e19
Lucía García <garciaramirez.luc@gmail.com>
2025-08-19 18:26:46 +0200

    declararvariablesejercicio1

---
+++ declararVariablesEjercicio1Forma1/declararVariables.js
@@ -0,0 +1,5 @@
+let nombre = "Lucia";
+document.getElementById("mostrarNombreLucia").textContent = nombre;
+
+// se usa getElementById para acceder al elemento HTML con el id especificado (ver index.html
+// para ver la explicación por el lado del html)
