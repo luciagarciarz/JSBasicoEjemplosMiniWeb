318bb454fa73538cc1a634cec0fa21f683b5d855
Lucía García <garciaramirez.luc@gmail.com>
2025-08-20 17:55:09 +0200

    declaración de variables ejercicio 2 formas 1 y 2

--- declararVariablesEjercicio1Forma1/declararVariables.js
+++ declararVariablesEjercicio1Forma1/declararVariables.js
@@ -1,5 +1,5 @@
 let nombre = "Lucia";
 document.getElementById("mostrarNombreLucia").textContent = nombre;
 
-// se usa getElementById para acceder al elemento HTML con el id especificado (ver index.html
-// para ver la explicación por el lado del html)
+/*se usa getElementById para acceder al elemento HTML con el id especificado (ver index.html
+para ver la explicación por el lado del html)*/
---
+++ declararVariablesEjercicio2Forma1/edadDeUsuario.js
@@ -0,0 +1,5 @@
+let edad = 34;
+document.getElementById("mostraredad").textContent = edad;
+
+/* se usa getElementById para acceder al elemento HTML con el id especificado (ver index.html
+ para ver la explicación por el lado del html)*/
