dd0aa08eec86f79c889c931edae65af27c3f34e3
Lucía García <garciaramirez.luc@gmail.com>
2025-08-27 15:10:59 +0200

    variables y string

--- ejemplos/declararVariablesEjercicio1Forma1/declararVariables.js
+++
@@ -1,5 +0,0 @@
-let nombre = "Lucia";
-document.getElementById("mostrarNombreLucia").textContent = nombre;
-
-/*se usa getElementById para acceder al elemento HTML con el id especificado (ver index.html
-para ver la explicación por el lado del html)*/
